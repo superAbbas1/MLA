114ed17adaeb8611b115804b21818445182d8339
Abbas Mansoor <abbasmansoor456@gmail.com>
2024-12-23 09:50:27 +0500

    updated API route

--- frontend/src/pages/home/hero/Hero.jsx
+++ frontend/src/pages/home/hero/Hero.jsx
@@ -31,7 +31,7 @@ const Home = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://mla-six.vercel.app/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
